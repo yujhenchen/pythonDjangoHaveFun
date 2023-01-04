9174309b94f72ec760edd68d6d44b01ad1378cf3
yujhenchen <yujhen.chen93@gmail.com>
2023-01-05 04:59:35 +0800

    make setup script nicer, add html homework practices

--- setup.sh
+++ setup.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-MY_ENV="myenv"
+MY_ENV="python_django_env"
 
-conda update -y conda
+# conda update -y conda
+conda update -n base -c defaults conda
 
 conda --version
 conda env list
 
-conda create --name myenv -y
-conda env list
+ENVS=$(conda env list)
+if [[ $ENVS = *"$MY_ENV"* ]]; then
+    echo "${MY_ENV} exists, will not create a new env"
+else
+    echo "${MY_ENV} does not exist, will create a new env"
+    conda create --name $MY_ENV -y
+    
+    # Install package
+    conda install -c conda-forge --name $MY_ENV django -y
+fi 
 
-conda activate myenv
+conda activate $MY_ENV
 conda env list
-
-# Install package
-conda install -c conda-forge --name $MY_ENV django -y
 
 conda list
