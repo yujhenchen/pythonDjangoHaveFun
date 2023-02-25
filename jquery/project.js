d0651027243c0a49542623f27d758f1280f46720
yujhenchen <yujhen.chen93@gmail.com>
2023-02-25 16:10:43 +0800

    implement click show colors

--- jquery/project.js
+++ jquery/project.js
@@ -1,15 +1,23 @@
 class CountFour {
   currentPlayer = 0;
   currentPlacedMap = new Map();
+  colorMap = new Map();
   // private placedMap: { [key: {number, number}]: string } = {};
 
   constructor(columnCount, rowCount) {
     this.resetGame(columnCount, rowCount);
+    this.initColorMap();
+  }
+
+  initColorMap() {
+    this.colorMap.set(0, "yellow");
+    this.colorMap.set(1, "blue");
   }
 
   getPlayerPlace(col) {
     this.currentPlacedMap.set(col, this.currentPlacedMap.get(col) - 1);
     // alert(this.currentPlacedMap.get(col));
+    this.switchPlayer();
     return { colIdex: col, rowIdex: this.currentPlacedMap.get(col) };
   }
 
@@ -22,7 +30,6 @@ class CountFour {
 
   checkWin() {
     // if anyone wins, resetGame
-    // else, switch player
   }
 
   switchPlayer() {
@@ -40,6 +47,22 @@ const countFour = new CountFour(columnCount, rowCount);
 $(".board td").click(function () {
   // alert(this.cellIndex); // 0, 1, 2, 3, 4, 5, 6
   const position = countFour.getPlayerPlace(this.cellIndex);
-  alert(`position: ${position.colIdex} ${position.rowIdex}`);
+  // alert(`position: ${position.colIdex} ${position.rowIdex}`);
+
+  // place
+  const td = $(".board tr")
+    .eq(position.rowIdex)
+    .find("td")
+    .eq(position.colIdex);
+
+  const button = td.find("button");
+
+  // alert(countFour.currentPlayer);
+  // alert(countFour.colorMap.get(countFour.currentPlayer));
+  button.css(
+    "background-color",
+    countFour.colorMap.get(countFour.currentPlayer)
+  );
+
   countFour.checkWin();
 });
