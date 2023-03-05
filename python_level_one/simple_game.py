79f8454041d63f75a9af12eb17a7f8fc28aefb01
yujhenchen <yujhen.chen93@gmail.com>
2023-03-05 12:17:02 +0800

    add basic files, add settings

---
+++ python_level_one/simple_game.py
@@ -0,0 +1,13 @@
+class GuessNumbers:
+    def __init__(self) -> None:
+        pass
+
+    def show_game_result(self) -> None:
+        pass
+
+    def make_guess(self) -> None:
+        pass
+
+
+if __name__ == "__main__":
+    print()
---
+++ python_level_one/test_simple_game.py
@@ -0,0 +1,28 @@
+import unittest
+
+""" 
+test cases for the target 123:
+
+123 -> Perfect Match
+
+134 -> 1 Match, 1 Close
+
+132 -> 1 Match, 2 Close
+
+145 -> 1 Match
+
+124 -> 2 Match
+
+425 -> 1 Close
+
+234 -> 2 Close
+
+231 -> 3 Close
+
+456 -> Nope
+
+"""
+
+
+class TestGuessNumbers(unittest.TestCase):
+    pass
